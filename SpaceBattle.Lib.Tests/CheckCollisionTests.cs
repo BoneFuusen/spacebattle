036f246725f162842bcb3a4a6215c4645285bd1a
BoneFuusen <kotov_sergey04@mail.ru>
2023-12-29 02:44:41 +0600

    Выполнил задание.

---
+++ SpaceBattle.Lib.Tests/CheckCollisionTests.cs
@@ -0,0 +1,57 @@
+using SpaceBattle.Lib;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+namespace CheckCollisionTests;
+
+public class CheckCollisionTests
+{
+    public CheckCollisionTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.GetProperty", (object[] args) => new List<int> { 1, 1, 1 }).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "IUObject.CheckCollision", (object[] args) => new CheckCollision((IUObject)args[0], (IUObject)args[1])).Execute();
+    }
+
+    [Fact]
+    public void NormalCheckCollision()
+    {
+        var mockICommand = new Mock<SpaceBattle.Lib.ICommand>();
+        var mockDict = new Mock<IDictionary<int, object>>();
+        var mockObj = new Mock<IUObject>();
+
+        mockICommand.Setup(m => m.Execute()).Verifiable();
+        mockDict.SetupGet(m => m[It.IsAny<int>()]).Returns(mockDict.Object);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CheckCollision", (object[] args) => mockDict.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Collision", (object[] args) => mockICommand.Object).Execute();
+
+        var checkCollision = IoC.Resolve<SpaceBattle.Lib.ICommand>("IUObject.CheckCollision", mockObj.Object, mockObj.Object);
+
+        checkCollision.Execute();
+
+        mockICommand.Verify();
+    }
+
+    [Fact]
+    public void notNormalCheckCollision()
+    {
+        var mockICommand = new Mock<SpaceBattle.Lib.ICommand>();
+        var mockDict = new Mock<IDictionary<int, object>>();
+        var mockObj = new Mock<SpaceBattle.Lib.IUObject>();
+
+        mockICommand.Setup(m => m.Execute()).Verifiable();
+        mockDict.SetupGet(m => m[It.IsAny<int>()]).Throws<Exception>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CheckCollision", (object[] args) => mockDict.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Collision", (object[] args) => mockICommand.Object).Execute();
+
+        var checkCollision = IoC.Resolve<SpaceBattle.Lib.ICommand>("IUObject.CheckCollision", mockObj.Object, mockObj.Object);
+
+        checkCollision.Execute();
+        mockICommand.Verify(m => m.Execute(), Times.Never());
+    }
+}
---
+++ SpaceBattle.Lib/CheckCollision.cs
@@ -0,0 +1,39 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CheckCollision : ICommand
+{
+    private readonly IUObject obj1, obj2;
+    private bool collision = true;
+
+    public CheckCollision(IUObject uobj1, IUObject uobj2)
+    {
+        obj1 = uobj1;
+        obj2 = uobj2;
+    }
+
+    public void Execute()
+    {
+        var builddecisiontree = IoC.Resolve<IDictionary<int, object>>("Game.CheckCollision");
+
+        var object1 = IoC.Resolve<List<int>>("Game.Commands.GetProperty", obj1);
+        var object2 = IoC.Resolve<List<int>>("Game.Commands.GetProperty", obj2);
+
+        var list = object1.Zip(object2, (a, b) => a - b).ToList();
+
+        try
+        {
+            list.ForEach(n => builddecisiontree = (IDictionary<int, object>)builddecisiontree[n]);
+        }
+        catch
+        {
+            collision = false;
+        }
+
+        if (collision == true)
+        {
+            IoC.Resolve<ICommand>("Game.Collision", obj1, obj2).Execute();
+        }
+    }
+}
