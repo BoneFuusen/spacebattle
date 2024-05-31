ff0fac265d009e18a92ced17005716a3b44defe9
Fink Eduard <fink.2004@mail.ru>
2024-05-31 18:36:10 +0600

    feat: Create Empty Game Objects

---
+++ SpaceBattle.Lib/CreateEmptyGameObjects.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CreateEmptyGameObjects : ICommand
+{
+    private int _quantity;
+    public CreateEmptyGameObjects(int quantity)
+    {
+        _quantity = quantity;
+    }
+
+    public void Execute()
+    {
+        var uObjects = IoC.Resolve<IDictionary>("Game.GetUObjects");
+
+        Enumerable.Range(0, _quantity).ToList().ForEach(i => {
+            uObjects.Add(i, IoC.Resolve<IUObject>("Game.UObject.Create"));
+        });
+    }
+}
